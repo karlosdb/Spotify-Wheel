9f850ae543b31cfeebd575cd2b673ba4b8363a02
Kevin Nguyen <ktnguy20@gmail.com>
2022-11-09 23:52:06 -0500

    api calls

---
+++ public/js/comments.js
@@ -0,0 +1,14 @@
+let currentUrl = window.location.href
+
+let arr = currentUrl.split('/')
+arr = arr.slice(0, -1)
+
+console.log(arr)
+
+currentUrl = arr.join('/')
+
+document.getElementById('submit-button').addEventListener('click', async () => {
+  const response = await fetch(`${currentUrl}/api/save_comment`);
+  const data = await response.json();
+  console.log(data);
+})
