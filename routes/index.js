a5d4c828e2f57134b3fcffbf7d181794db11846f
Kevin Nguyen <ktnguy20@gmail.com>
2022-11-09 19:57:06 -0500

    static

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+app.use(express.static("public"))
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile('/app/index.html');
