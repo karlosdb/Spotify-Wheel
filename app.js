3270211ce6e80a07d46a0066351f1717863d46af
Kevin Nguyen <ktnguy20@gmail.com>
2022-11-09 20:11:24 -0500

    root

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(function(err, req, res, next) {
 });
 
 app.get('/', function(req, res, next) {
-  res.sendFile('/html/index.html');
+  res.sendFile('html/index.html');
 });
 app.get('/styles.css', function(req, res, next) {
-  res.sendFile('/stylesheets/styles.css');
+  res.sendFile('stylesheets/styles.css');
 });
 
 let port = process.env.PORT;
