9e252609ce2ef8d41a9ca694230063790f714c0f
Kevin Nguyen <ktnguy20@gmail.com>
2022-11-09 20:41:06 -0500

    working static pages and paths

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var app = express();
 
-// view engine setup
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res, next) {
@@ -22,6 +20,9 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(port);
+
+app.listen(port, () => {
+  console.log(`Spotify Wheel listening on port ${port}`)
+});
 
 module.exports = app;
