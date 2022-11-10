71fa58af22b69c91ae02ec5c9d18488e8ef9c420
Kevin Nguyen <ktnguy20@gmail.com>
2022-11-09 20:09:59 -0500

    static

--- app.js
+++ app.js
@@ -3,24 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('../../routes/index');
-var usersRouter = require('../../routes/users');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,6 +29,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.get('/', function(req, res, next) {
+  res.sendFile('/html/index.html');
+});
+app.get('/styles.css', function(req, res, next) {
+  res.sendFile('/stylesheets/styles.css');
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
