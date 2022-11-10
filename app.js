a3658221b43e9417b57046e4f0b2c127c7e0d11c
Kevin Nguyen <ktnguy20@gmail.com>
2022-11-09 20:35:52 -0500

    remove error handlers

--- app.js
+++ app.js
@@ -6,22 +6,6 @@ var app = express();
 // view engine setup
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 app.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
