ac1d0d70281748b3b7a882032dbf03980ebca5ed
Karlos <karlos@deliberate.solutions>
2022-12-10 23:24:56 -0500

    added ability to connect/auth spotify login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const data = require('./data')
 var express = require('express');
 var path = require('path');
 var app = express();
+var SpotifyWebApi = require('spotify-web-api-node');
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
@@ -17,9 +18,85 @@ if (!process.env.URI) {
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// SPOTIFY SHITTTTTTTTTTTT
+const scopes = [
+  'ugc-image-upload',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing',
+  'streaming',
+  'app-remote-control',
+  'user-read-email',
+  'user-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-read-private',
+  'playlist-modify-private',
+  'user-library-modify',
+  'user-library-read',
+  'user-top-read',
+  'user-read-playback-position',
+  'user-read-recently-played',
+  'user-follow-read',
+  'user-follow-modify'
+];
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: '9923775e49b54d5dad3b5b291d7790b5',
+  clientSecret: '2fd4c770741642628cdf09fc522969ea',
+  redirectUri: 'http://localhost:8000/callback'
+});
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/spotifyLogin', (req, res) => {
+  res.redirect(spotifyApi.createAuthorizeURL(scopes));
+});
+
+app.get('/callback', (req, res) => {
+  const error = req.query.error;
+  const code = req.query.code;
+  const state = req.query.state;
+
+  if (error) {
+    console.error('Callback Error:', error);
+    res.send(`Callback Error: ${error}`);
+    return;
+  }
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      const access_token = data.body['access_token'];
+      const refresh_token = data.body['refresh_token'];
+      const expires_in = data.body['expires_in'];
+
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+
+      console.log('access_token:', access_token);
+      console.log('refresh_token:', refresh_token);
+
+      console.log(
+        `Sucessfully retreived access token. Expires in ${expires_in} s.`
+      );
+      res.send('Success! You can now close the window.');
+
+      setInterval(async () => {
+        const data = await spotifyApi.refreshAccessToken();
+        const access_token = data.body['access_token'];
+
+        console.log('The access token has been refreshed!');
+        console.log('access_token:', access_token);
+        spotifyApi.setAccessToken(access_token);
+      }, expires_in / 2 * 1000);
+    })
+    .catch(error => {
+      console.error('Error getting Tokens:', error);
+      res.send(`Error getting Tokens: ${error}`);
+    });
+});
 
 app.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, 'public/login.html'));
